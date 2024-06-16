9bcd18ca9e789de1c15293d04c376944a31ad4f3
SPKchrono <spkchrono@proton.me>
2024-06-16 03:01:07 +0300

    Add files via upload

---
+++ slstatus/config.mk
@@ -0,0 +1,32 @@
+NAME = slstatus
+VERSION = 1.0
+
+# Customize below to fit your system
+
+# paths
+
+# See LICENSE file for copyright and license details.
+
+PREFIX = /usr
+MANPREFIX = ${PREFIX}/share/man
+
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
+
+# includes and libs
+INCS = -I. -I/usr/include -I${X11INC}
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lasound
+
+# flags
+CPPFLAGS = -DVERSION=\"${VERSION}\" -D_GNU_SOURCE
+CFLAGS = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
+#CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+LDFLAGS = -g ${LIBS}
+#LDFLAGS = -s ${LIBS}
+
+# Solaris
+#CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
+#LDFLAGS = ${LIBS}
+
+# compiler and linker
+CC = cc
